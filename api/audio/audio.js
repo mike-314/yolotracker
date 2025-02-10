f06987bd40b2cd8dadce07998ae173215763dd48
Your Name <you@example.com>
2025-02-10 02:22:27 +0100

    fix function.json in api/audio

--- api/audio/audio.js
+++ api/audio/audio.js
@@ -1,8 +1,13 @@
 module.exports = async function (context, req) {
+    const audioSource = process.env.AUDIO_SOURCE || "https://azuremkdevst.blob.core.windows.net/audio/Risen_Soundtrack_02_The_Beach.m4a";
+
     context.res = {
         status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: JSON.stringify({
-            audioSource: process.env.AUDIO_SOURCE || "https://azuremkdevst.blob.core.windows.net/audio/Risen_Soundtrack_02_The_Beach.m4a"
+            audioSource: audioSource
         })
     };
 };
